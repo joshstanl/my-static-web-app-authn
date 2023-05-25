c80d1eb5f21c2a51c87ab18afaa20c9995a293a9
Joshua Stanley <Joshua.Stanley@corrohealth.com>
2023-05-25 16:10:11 -0500

    Modifications to get vue working

--- vue-app/vue.config.js
+++ vue-app/vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:7071',
+        target: 'http://127.0.0.1:3001',
         ws: true,
         changeOrigin: true,
       },
